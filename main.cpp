68cfdec961b8f103ec14d890ab4ef8e9fdea6465
ErmakovaPA <YermakovaPA@mpei.ru>
2022-03-23 02:33:57 +0300

    code: Добавлен поиск количества значений попадающих в интервал

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <thread>
 using namespace std::chrono;
 using namespace std::this_thread;
-
+//ввод
 void input (size_t &sensor,size_t &count_surveys,size_t &time_interval,size_t &count_intervals ){
     sensor = 0, count_surveys = 0, time_interval = 0, count_intervals = 0;
     std::cout << "Enter the sensor number :" << std::endl;
@@ -17,6 +17,22 @@ void input (size_t &sensor,size_t &count_surveys,size_t &time_interval,size_t &c
     std::cin >> time_interval;
     std::cout << "Enter the number of intervals :"<< std::endl;
     std::cin >> count_intervals;
+    return;
+}
+//поиск количества значений попадающих в интервал
+void hit_rate(std::vector<double>values, size_t count_surveys, std::vector<size_t> &hit_rates, size_t count_intervals, double lenght_interval,double min){
+    for(size_t i = 0; i < count_surveys; i++){
+            bool found = false;
+            for(size_t count = 0; count < (count_intervals - 1) && !found; count++){
+                auto lo = min + (count * lenght_interval);
+                auto hi = min + ((count + 1)* lenght_interval);
+                if ( (values[i] >= lo) && (values[i] < hi)) {
+                    found = true;
+                    hit_rates[i]++;
+                }
+            }
+            if(!found) hit_rates[count_intervals - 1]++;
+    }
     return;
 }
 
@@ -37,14 +53,16 @@ int main (){
         if ((i + 1) % 5 == 0 && i != 0) std::cout << std::endl;
     }
     //расчет
-    //1. найдем минимум и максимум
+    //1. поиск минимум и максимум
     double min = sensor_values[0];
     double max = sensor_values[0];
     for (double value : sensor_values){
         if (min > value) min = value;
         if (max < value) max = value;
     }
-
-
+    double lenght_interval = 0;
+    std::vector<size_t>hit_rates(count_intervals,0);
+    lenght_interval = (max - min)/count_intervals;
+    hit_rate(sensor_values,count_surveys, hit_rates, count_intervals, lenght_interval,min);
     return 0;
 }
