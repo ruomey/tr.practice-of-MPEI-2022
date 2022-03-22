4e190610c9369f7a4ed0ca9455003f12910a6ac7
ErmakovaPA <YermakovaPA@mpei.ru>
2022-03-22 22:30:36 +0300

    code: добавлена процедура ввода данных

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 #include "plant.h"
 #include <vector>
 #include <math.h>
-int main (){
-    size_t sensor = 0, number_of_surveys = 0, time_interval = 0, number_of_intervals = 0;
+
+void input (size_t &sensor,size_t &number_of_surveys,size_t &time_interval,size_t &number_of_intervals ){
+    sensor = 0, number_of_surveys = 0, time_interval = 0, number_of_intervals = 0;
     std::cout << "Enter the sensor number :" << std::endl;
     std::cin >> sensor;
     std::cout << "Enter the number of surveys :" << std::endl;
@@ -12,6 +13,12 @@ int main (){
     std::cin >> time_interval;
     std::cout << "Enter the number of intervals :"<< std::endl;
     std::cin >> number_of_intervals;
+    return;
+}
+
+int main (){
+    size_t sensor, number_of_surveys,time_interval, number_of_intervals;
+    input(sensor, number_of_surveys, time_interval, number_of_intervals);
     std::cout << sensor << number_of_surveys << time_interval << number_of_intervals;
     return 0;
 }
